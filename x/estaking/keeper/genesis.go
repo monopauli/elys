543aad5bcc73e9c380a1b1e4605a34b27b8a5ce9
monopauli <steamleopac@gmail.com>
2024-11-29 10:27:05 +0100

    eden validator hooks

--- x/estaking/keeper/genesis.go
+++ x/estaking/keeper/genesis.go
@@ -2,26 +2,28 @@ package keeper
 
 import (
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	authtypes "github.com/cosmos/cosmos-sdk/x/auth/types"
 	"github.com/elys-network/elys/x/estaking/types"
+	ptypes "github.com/elys-network/elys/x/parameter/types"
 )
 
 // InitGenesis initializes the module's state from a provided genesis state.
 func (k Keeper) InitGenesis(ctx sdk.Context, genState types.GenesisState) {
-	// var shouldRunEdenValHook = false
-	// var shouldRunEdenBValHook = false
-	// edenValAddr := sdk.ValAddress(authtypes.NewModuleAddress(ptypes.Eden))
-	// edenBValAddr := sdk.ValAddress(authtypes.NewModuleAddress(ptypes.EdenB))
+	//var shouldRunEdenValHook = false
+	//var shouldRunEdenBValHook = false
+	edenValAddr := sdk.ValAddress(authtypes.NewModuleAddress(ptypes.Eden))
+	edenBValAddr := sdk.ValAddress(authtypes.NewModuleAddress(ptypes.EdenB))
 
-	// // Create validators for Eden and EdenB
-	// if genState.Params.EdenCommitVal == "" {
-	// 	genState.Params.EdenCommitVal = edenValAddr.String()
-	// 	shouldRunEdenValHook = true
-	// }
+	// Create validators for Eden and EdenB
+	if genState.Params.EdenCommitVal == "" {
+		genState.Params.EdenCommitVal = edenValAddr.String()
+		//shouldRunEdenValHook = true
+	}
 
-	// if genState.Params.EdenbCommitVal == "" {
-	// 	genState.Params.EdenbCommitVal = edenBValAddr.String()
-	// 	shouldRunEdenBValHook = true
-	// }
+	if genState.Params.EdenbCommitVal == "" {
+		genState.Params.EdenbCommitVal = edenBValAddr.String()
+		//shouldRunEdenBValHook = true
+	}
 
 	// this line is used by starport scaffolding # genesis/module/init
 	k.SetParams(ctx, genState.Params)
